dacd928e19946d564060b5ea5a053c0adb619dea
manishsaxenavidealpha <manish.saxena@videalpha.com>
2025-02-20 01:01:28 +0530

    web only

--- lib/constants/strings_constants.dart
+++
@@ -1,9 +0,0 @@
-class AppStringConstants {
-  static const String appTitle = "Web App";
-  static const String imageUrl = "Image URL";
-  static const String appName = "Flutter App";
-  static const String exitFullscreen = "Exit fullscreen";
-  static const String enterFullscreen = "Enter fullscreen";
-  static const String invalidImageUrl = "Invalid Image URL";
-  static const String errorMssg = "Something went wrong!!";
-}
